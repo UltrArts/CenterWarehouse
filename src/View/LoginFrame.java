33829ece86441fbd46fdba5e7fba6b67a735c3d1
Edson-UltraDev <Edson-UltraDev@DESKTOP-AP7F5CV>
2022-10-31 16:59:37 +0200

    IncreaseChanges

--- src/View/LoginFrame.java
+++ src/View/LoginFrame.java
@@ -27,11 +27,11 @@ public class LoginFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 316, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 273, Short.MAX_VALUE)
         );
 
         pack();
